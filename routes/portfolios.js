09f4ea7f8f07a9fb9fc061565fa332f06720688d
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-04 23:45:56 -0300

    feat: function to get portfolio by id

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { getPortfolios } = require('../controllers/portfolios');
+const {
+  getPortfolios,
+  getPortfolioBydId,
+} = require('../controllers/portfolios');
 
 router.get('/', getPortfolios);
+router.get('/:id', getPortfolioBydId);
 
 module.exports = router;
