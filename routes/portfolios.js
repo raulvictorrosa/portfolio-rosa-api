e1d1f2f936fd50f1c14a1946119a26cdb76d6e82
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-14 21:10:01 -0300

    feat: route to update portfolio

--- routes/portfolios.js
+++ routes/portfolios.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const {
   getPortfolios,
   getPortfolioBydId,
-  createPortfolio
+  createPortfolio,
+  updatePortfolio
 } = require('../controllers/portfolios')
 const { checkJwt } = require('../controllers/auth')
 
@@ -11,5 +12,7 @@ router.get('/', getPortfolios)
 router.get('/:id', getPortfolioBydId)
 
 router.post('/', checkJwt, createPortfolio)
+
+router.patch('/:id', checkJwt, updatePortfolio)
 
 module.exports = router
