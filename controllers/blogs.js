17d181deff2d8b6c1fa24585faf704538ed1978b
Raul Rosa <raulvictorrosa@gmail.com>
2020-11-22 20:44:42 -0300

    feat: blog controller to update blog

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -42,3 +42,28 @@ exports.createBlog = async (req, res) => {
     return res.status(422).json(error)
   }
 }
+
+exports.updateBlog = (req, res) => {
+  const {
+    body,
+    params: { id }
+  } = req
+
+  Blog.findById(id, async (err, blog) => {
+    if (err) {
+      return res.status(422).send(error.message)
+    }
+
+    // TODO: Check if user is publishing blog and if user is publishing then create SLUG
+
+    blog.set(body)
+    blog.updateAt = new Date()
+
+    try {
+      const updatedBlog = await blog.save()
+      return res.json(updatedBlog)
+    } catch (error) {
+      return res.status(422).send(error.message)
+    }
+  })
+}
